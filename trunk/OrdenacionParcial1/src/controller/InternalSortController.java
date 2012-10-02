dbed3ddfba0aa54bc487943f968e70dc5538a6b8
arandilopez.93@gmail.com <arandilopez.93@gmail.com@e8b98b60-9f92-f88c-d90e-de549a1f8eb0>
2012-10-02 20:49:18 +0000

    Modifcacion a InternalSortController

--- trunk/OrdenacionParcial1/src/controller/InternalSortController.java
+++ trunk/OrdenacionParcial1/src/controller/InternalSortController.java
@@ -11,6 +11,7 @@ import model.InternalSort;
  * @author ArandiLopez
  */
 public class InternalSortController {
+
     String[] ejemplo = new String[20];//atributo que se debera modificar por el que el DAO recibe.
     InternalSort ordenador = new InternalSort();
 
@@ -21,46 +22,74 @@ public class InternalSortController {
     }
 
     /**
-     * 
-     * @return ordenador 
+     *
+     * @return ordenador
      */
     public InternalSort getOrdenador() {
         return ordenador;
     }
 
     /**
-     * 
+     *
      * @param ordenador
      */
     public void setOrdenador(InternalSort ordenador) {
         this.ordenador = ordenador;
     }
-    
-    public double getMilisInsercion(){
-        double t0,t1;
+
+    /**
+     * Metodo para obtner el tiempo de duracion del proceso de ordenamiento de
+     * insercion.
+     *
+     * @return t1
+     */
+    public double getMilisInsercion() {
+        double t0, t1;
         t0 = System.currentTimeMillis();
         ordenador.insercion(ejemplo);
-        t1=System.currentTimeMillis() - t0;
+        t1 = System.currentTimeMillis() - t0;
         return t1;
     }
-    
-    public double getMilisBurbujaMayor(){
-        double t0,t1;
+
+    /**
+     * Metodo para obtner el tiempo de duracion del proceso de ordenamiento de
+     * Burbuja mayor.
+     *
+     * @return t1
+     */
+    public double getMilisBurbujaMayor() {
+        double t0, t1;
         t0 = System.currentTimeMillis();
         ordenador.burbujaMayor(ejemplo);
-        t1=System.currentTimeMillis() - t0;
+        t1 = System.currentTimeMillis() - t0;
         return t1;
     }
-    
-    public double getMilisBurbujaMenor(){
-        double t0,t1;
+
+    /**
+     * Metodo para obtner el tiempo de duracion del proceso de ordenamiento de
+     * Burbuja menor.
+     *
+     * @return
+     */
+    public double getMilisBurbujaMenor() {
+        double t0, t1;
         t0 = System.currentTimeMillis();
         ordenador.burbujaMenor(ejemplo);
-        t1=System.currentTimeMillis() - t0;
+        t1 = System.currentTimeMillis() - t0;
         return t1;
     }
-    
-    
-    
-    
+
+    /**
+     * Metodo para obtner el tiempo de duracion del proceso de ordenamiento de
+     * Shellsort
+     *
+     * @return
+     */
+    public double getMilisShell() {
+        double t0, t1;
+        t0 = System.currentTimeMillis();
+        ordenador.ShellSort(ejemplo);
+        t1 = System.currentTimeMillis() - t0;
+        return t1;
+    }
 }//fin de la clase
