55743100ee656097d735569ffb9e3a2a63ff7a42
noe.cob18@gmail.com <noe.cob18@gmail.com@e8b98b60-9f92-f88c-d90e-de549a1f8eb0>
2012-10-03 18:42:11 +0000

    DAO

--- trunk/OrdenacionParcial1/src/DAO/DAO.java
+++ trunk/OrdenacionParcial1/src/DAO/DAO.java
@@ -2,7 +2,6 @@ package DAO;
 
 import java.util.StringTokenizer;
 import java.io.*;
-import javax.swing.JFileChooser;
 
 /**
  *
@@ -10,6 +9,56 @@ import javax.swing.JFileChooser;
  */
 public class DAO {
 
-   
+    private int sizeArray;
+    String path;
+
+    public String[] leer(File toRead) {
+        this.path = toRead.getAbsolutePath();
+        String[] array = null;
+        try {
+            FileReader reader = new FileReader(toRead);
+            BufferedReader bufer = new BufferedReader(reader);
+            String aux;
+            while ((aux = bufer.readLine()) != null) {
+                StringTokenizer tokenizer = new StringTokenizer(aux, " .,;:¿?¡!=");
+                while (tokenizer.hasMoreTokens()) {
+                    if (tokenizer.nextToken().length() > 3) {
+                        array[sizeArray] = tokenizer.nextToken();
+                    }
+                    sizeArray++;
+                }
+            }
+            bufer.close();
+            reader.close();
+
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+        
+        return array;
+
+    }//fin de la clase leer
+    
+    
+    public void escribir(String[] array){
+        File separado= new File("separado.txt");
+        try {
+            FileWriter writer = new FileWriter(separado);
+            BufferedWriter bufer = new BufferedWriter(writer);
+            
+            for(int i=0 ; i<array.length;i++){
+                bufer.write(array[i]);
+                bufer.newLine();
+            }
+            
+            bufer.close();
+            writer.close();
+            
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+        
+    }
+    
     
 }
