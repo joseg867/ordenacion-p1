b5c7b86cd27d16ccd99b669efdc418de6fa4c427
noe.cob18 <noe.cob18@gmail.com>
2012-10-03 17:52:24 +0000

    DAO

--- OrdenacionParcial1/src/DAO/PruebaDao.java
+++ OrdenacionParcial1/src/DAO/PruebaDao.java
@@ -1,5 +1,6 @@
 package DAO;
 
+
 /**
  *
  * @author Noe
@@ -10,9 +11,8 @@ public class PruebaDao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-       // DAO a = new DAO();
-       // String l=a.leer();
-       // System.out.println(l);
+//         TODO code application logic here
+//      
+        
     }
 }
