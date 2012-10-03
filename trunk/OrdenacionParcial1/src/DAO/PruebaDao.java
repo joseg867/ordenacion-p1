7d7357bc32b080d8f52fab7097150c0d2267d67c
noe.cob18@gmail.com <noe.cob18@gmail.com@e8b98b60-9f92-f88c-d90e-de549a1f8eb0>
2012-10-03 19:29:12 +0000

    DAO

--- trunk/OrdenacionParcial1/src/DAO/PruebaDao.java
+++ trunk/OrdenacionParcial1/src/DAO/PruebaDao.java
@@ -1,5 +1,8 @@
 package DAO;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+
 
 /**
  *
@@ -12,7 +15,19 @@ public class PruebaDao {
      */
     public static void main(String[] args) {
 //         TODO code application logic here
-//      
+        
+        JFileChooser chooser = new JFileChooser();
+        chooser.showOpenDialog(null);
+        chooser.setCurrentDirectory(new File("./"));
+        DAO dao = new DAO();
+          
+        try {
+            String[] hola = dao.leer(chooser.getSelectedFile());
+            
+            //dao.escribir(hola);
+        } catch (Exception e) {
+            System.out.println("Chinga tu Madre");
+        }
         
     }
 }
