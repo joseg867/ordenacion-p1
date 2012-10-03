6a9a744df3228e88bb595fd39822df89c6e19dd4
noe.cob18@gmail.com <noe.cob18@gmail.com@e8b98b60-9f92-f88c-d90e-de549a1f8eb0>
2012-10-03 17:52:24 +0000

    DAO

--- trunk/OrdenacionParcial1/src/DAO/DAO.java
+++ trunk/OrdenacionParcial1/src/DAO/DAO.java
@@ -10,63 +10,6 @@ import javax.swing.JFileChooser;
  */
 public class DAO {
 
-    private File rootFile;
-    private int sizeArray;
-
-    public int getSizeArray() {
-        return sizeArray;
-    }
-
-    public void setSizeArray(int sizeArray) {
-        this.sizeArray = sizeArray;
-    }
-
-    public DAO() {
-    }
-
-    public String[] leer(File rootFile, int sizeArray) {
-
-        try {
-            String path = rootFile.getAbsolutePath();
-
-            try {
-                FileReader fr = new FileReader(rootFile);
-                BufferedReader br = new BufferedReader(fr);
-                String aux;
-                while ((aux = br.readLine()) != null) {
-                    //lectura = lectura + aux + "\n";
-                    StringTokenizer st = new StringTokenizer(aux," ");
-                    String[] arreglo = new String[st.countTokens() + 1];
-
-                    while (st.hasMoreElements()) {
-                        arreglo[sizeArray] = st.nextToken();
-                        System.out.println(arreglo[sizeArray]);
-                        sizeArray++;
-
-                    }
-
-                }
-            } catch (IOException e) {
-            }
-
-        } catch (NullPointerException e) {
-            javax.swing.JOptionPane.showMessageDialog(null, e);
-
-        }
-        return arreglo;
-    }
-
-    /**
-     * @return the rootFile
-     */
-    public File getRootFile() {
-        return rootFile;
-    }
-
-    /**
-     * @param rootFile the rootFile to set
-     */
-    public void setRootFile(File rootFile) {
-        this.rootFile = rootFile;
-    }
+   
+    
 }
